73e6601c8b2551e5715667a4dd5d61024b1ccec8
newrav1k <kritsky.academi@gmail.com>
2025-09-05 01:53:39 +0300

    Added exceptions and handler

---
+++ account-service/src/main/java/ru/mirea/newrav1k/accountservice/controller/handler/AccountExceptionHandler.java
@@ -0,0 +1,39 @@
+package ru.mirea.newrav1k.accountservice.controller.handler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.mirea.newrav1k.accountservice.exception.AccountNotFoundException;
+import ru.mirea.newrav1k.accountservice.exception.InsufficientBalanceException;
+
+import java.util.Locale;
+
+@Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class AccountExceptionHandler {
+
+    private final MessageSource messageSource;
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<ProblemDetail> handleAccountNotFoundException(AccountNotFoundException exception,
+                                                                        Locale locale) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND,
+                this.messageSource.getMessage(exception.getMessage(), new Object[0], "account.not.found", locale));
+        return ResponseEntity.of(problemDetail).build();
+    }
+
+    @ExceptionHandler(InsufficientBalanceException.class)
+    public ResponseEntity<ProblemDetail> handleInsufficientBalanceException(InsufficientBalanceException exception,
+                                                                            Locale locale) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                this.messageSource.getMessage(exception.getMessage(), new Object[0], "insufficient.balance", locale));
+        return ResponseEntity.of(problemDetail).build();
+    }
+
+}
---
+++ account-service/src/main/java/ru/mirea/newrav1k/accountservice/exception/InsufficientBalanceException.java
@@ -0,0 +1,25 @@
+package ru.mirea.newrav1k.accountservice.exception;
+
+public class InsufficientBalanceException extends RuntimeException {
+
+    public InsufficientBalanceException() {
+        super();
+    }
+
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+
+    public InsufficientBalanceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InsufficientBalanceException(Throwable cause) {
+        super(cause);
+    }
+
+    protected InsufficientBalanceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
