2396b2410e0fcc77000dba594e0474a9a4c9c00c
newrav1k <kritsky.academi@gmail.com>
2025-09-04 17:52:51 +0300

    Added internalization and a class with an expression code

---
+++ transaction-service/src/main/java/org/example/transactionservice/utils/MessageCode.java
@@ -0,0 +1,11 @@
+package org.example.transactionservice.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MessageCode {
+
+    public static final String TRANSACTION_NOT_FOUND = "transaction.not.found";
+
+}
