9fb25947448b1faf8278c92712f56eb9dd209cd9
newrav1k <kritsky.academi@gmail.com>
2025-09-04 18:05:11 +0300

    Added the account-service module

---
+++ account-service/src/main/java/ru/mirea/newrav1k/accountservice/AccountServiceApplication.java
@@ -0,0 +1,11 @@
+package ru.mirea.newrav1k.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+}
