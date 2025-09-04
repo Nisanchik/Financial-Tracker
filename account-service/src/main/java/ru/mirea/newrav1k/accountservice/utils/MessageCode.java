2891ed0d5c0326afb576490570082c54bbcdfe3d
newrav1k <kritsky.academi@gmail.com>
2025-09-05 01:54:39 +0300

    Added internationalization and a class with an expression code

---
+++ account-service/src/main/java/ru/mirea/newrav1k/accountservice/utils/MessageCode.java
@@ -0,0 +1,13 @@
+package ru.mirea.newrav1k.accountservice.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class MessageCode {
+
+    public static final String ACCOUNT_NOT_FOUND = "account.not.found";
+
+    public static final String INSUFFICIENT_BALANCE = "insufficient.balance";
+
+}
