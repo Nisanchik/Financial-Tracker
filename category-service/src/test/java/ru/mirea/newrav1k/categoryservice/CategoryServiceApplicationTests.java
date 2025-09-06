62525697642f0d27131915c245b6db2bb4988762
newrav1k <kritsky.academi@gmail.com>
2025-09-06 22:55:05 +0300

    Added applications classes to category-service

---
+++ category-service/src/test/java/ru/mirea/newrav1k/categoryservice/CategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mirea.newrav1k.categoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
