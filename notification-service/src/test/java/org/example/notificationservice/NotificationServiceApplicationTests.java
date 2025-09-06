8ef31b80f199e392748cb789a23523ef29310631
newrav1k <kritsky.academi@gmail.com>
2025-09-06 22:36:39 +0300

    The package in the notification-service has been changed

--- notification-service/src/test/java/org/example/notificationservice/NotificationServiceApplicationTests.java
+++ notification-service/src/test/java/org/example/notificationservice/NotificationServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.notification_service;
+package org.example.notificationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
