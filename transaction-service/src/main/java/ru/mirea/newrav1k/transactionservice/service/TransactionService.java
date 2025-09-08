a92c333aa99b9b3299b93b293346a26d69bf0b66
newrav1k <kritsky.academi@gmail.com>
2025-09-08 23:19:22 +0300

    Logging to the TransactionService has been fixed and minor changes have been made

--- transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/service/TransactionService.java
+++ transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/service/TransactionService.java
@@ -6,7 +6,13 @@ import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.mirea.newrav1k.transactionservice.exception.TransactionNotFoundException;
+import ru.mirea.newrav1k.transactionservice.exception.TransactionProcessingException;
 import ru.mirea.newrav1k.transactionservice.exception.TransactionServiceException;
 import ru.mirea.newrav1k.transactionservice.mapper.TransactionMapper;
 import ru.mirea.newrav1k.transactionservice.model.dto.TransactionCreateRequest;
@@ -14,15 +20,12 @@ import ru.mirea.newrav1k.transactionservice.model.dto.TransactionFilter;
 import ru.mirea.newrav1k.transactionservice.model.dto.TransactionResponse;
 import ru.mirea.newrav1k.transactionservice.model.dto.TransactionUpdateRequest;
 import ru.mirea.newrav1k.transactionservice.model.entity.Transaction;
+import ru.mirea.newrav1k.transactionservice.model.enums.TransactionStatus;
 import ru.mirea.newrav1k.transactionservice.model.enums.TransactionType;
 import ru.mirea.newrav1k.transactionservice.repository.TransactionRepository;
 import ru.mirea.newrav1k.transactionservice.service.client.AccountClient;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 import static ru.mirea.newrav1k.transactionservice.utils.MessageCode.TRANSACTION_UPDATE_FAILED;
@@ -42,14 +45,14 @@ public class TransactionService {
     private final AccountClient accountClient;
 
     public Page<TransactionResponse> findAll(TransactionFilter filter, Pageable pageable) {
-        log.info("Finding all transactions by pageable");
+        log.debug("Request to get all transactions");
         Specification<Transaction> specification = this.transactionRepository.buildTransactionSpecification(filter);
         return this.transactionRepository.findAll(specification, pageable)
                 .map(this.transactionMapper::toTransactionResponse);
     }
 
     public TransactionResponse findById(UUID transactionId) {
-        log.info("Finding transaction with id {}", transactionId);
+        log.debug("Request to get transaction by id {}", transactionId);
         return this.transactionRepository.findById(transactionId)
                 .map(this.transactionMapper::toTransactionResponse)
                 .orElseThrow(TransactionNotFoundException::new);
@@ -57,20 +60,41 @@ public class TransactionService {
 
     @Transactional
     public TransactionResponse create(TransactionCreateRequest request) {
-        log.info("Creating new transaction");
+        log.debug("Request to create a new transaction");
+        Transaction transaction = savePendingTransaction(request);
+        try {
+            processUpdateBalance(transaction);
+            Transaction completedTransaction = saveCompletedTransaction(transaction);
+            return this.transactionMapper.toTransactionResponse(completedTransaction);
+        } catch (Exception exception) {
+            log.error("Transaction creation failed", exception);
+            throw new TransactionProcessingException();
+        }
+    }
+
+    private Transaction savePendingTransaction(TransactionCreateRequest request) {
         Transaction transaction = buildTransactionFromRequest(request);
-        this.transactionRepository.save(transaction);
-        processUpdateBalance(transaction);
-        return this.transactionMapper.toTransactionResponse(transaction);
+        transaction.setStatus(TransactionStatus.PENDING);
+        return this.transactionRepository.save(transaction);
+    }
+
+    private Transaction saveCompletedTransaction(Transaction transaction) {
+        transaction.setStatus(TransactionStatus.COMPLETED);
+        return this.transactionRepository.save(transaction);
     }
 
     @Transactional
     public TransactionResponse updateById(UUID transactionId, TransactionUpdateRequest request) {
-        log.info("Updating transaction with id {}", transactionId);
+        log.debug("Request to update transaction by id {}", transactionId);
         return this.transactionRepository.findById(transactionId)
                 .map(transaction -> {
-                    transaction.setDescription(request.description());
-                    transaction.setAmount(request.amount());
+                    if (request.description() != null) {
+                        transaction.setDescription(request.description());
+                    }
+                    if (request.amount() != null) {
+                        transaction.setAmount(request.amount());
+                        // TODO: реализовать компенсирующие действия при изменении
+                    }
                     return transaction;
                 })
                 .map(this.transactionMapper::toTransactionResponse)
@@ -79,12 +103,12 @@ public class TransactionService {
 
     @Transactional
     public TransactionResponse updateById(UUID transactionId, JsonNode jsonNode) {
-        log.info("Updating transaction with id {}", transactionId);
+        log.debug("Request to update transaction by id {}", transactionId);
         Transaction transaction = this.transactionRepository.findById(transactionId)
                 .orElseThrow(TransactionNotFoundException::new);
         try {
             this.objectMapper.readerForUpdating(transaction).readValue(jsonNode);
-
+            // TODO: реализовать компенсирующие действия при изменении
             return this.transactionMapper.toTransactionResponse(transaction);
         } catch (Exception exception) {
             log.error("Error while updating transaction with id {}", transactionId, exception);
@@ -94,12 +118,21 @@ public class TransactionService {
 
     @Transactional
     public void deleteById(UUID transactionId) {
-        log.info("Deleting transaction by id {}", transactionId);
-        this.transactionRepository.findById(transactionId)
-                .ifPresent(transaction -> {
-                    this.accountClient.updateBalance(transaction.getAccountId(), transaction.getAmount());
-                    this.transactionRepository.delete(transaction);
-                });
+        log.debug("Request to delete transaction by id {}", transactionId);
+        Transaction transaction = this.transactionRepository.findById(transactionId)
+                .orElseThrow(TransactionNotFoundException::new);
+        if (transaction.getStatus() == TransactionStatus.COMPLETED) {
+            try {
+                BigDecimal compensationAmount = transaction.getType() == TransactionType.INCOME
+                        ? transaction.getAmount().negate()
+                        : transaction.getAmount();
+                this.accountClient.updateBalance(transaction.getAccountId(), compensationAmount);
+            } catch (Exception exception) {
+                log.error("Error while deleting transaction by id {}", transactionId, exception);
+                throw new TransactionProcessingException();
+            }
+        }
+        this.transactionRepository.delete(transaction);
     }
 
     private Transaction buildTransactionFromRequest(TransactionCreateRequest request) {
