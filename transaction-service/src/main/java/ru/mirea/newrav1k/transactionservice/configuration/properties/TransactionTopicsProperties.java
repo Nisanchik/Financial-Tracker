3a3662df2ff91c012c0bf4ad9ed2a1c054018233
newrav1k <kritsky.academi@gmail.com>
2025-09-10 00:41:54 +0300

    Added topics to application.yaml and TransactionTopicsProperties

---
+++ transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/configuration/properties/TransactionTopicsProperties.java
@@ -0,0 +1,16 @@
+package ru.mirea.newrav1k.transactionservice.configuration.properties;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
+@ConfigurationProperties(prefix = "transaction-service.kafka.topics")
+public record TransactionTopicsProperties(
+        @NotBlank String transactionCreated,
+        @NotBlank String transactionCancelled,
+        @NotBlank String transactionSuccessfullyCreated,
+        @NotBlank String transactionBalanceFailure
+) {
+
+}
