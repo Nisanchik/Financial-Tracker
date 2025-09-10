ba4ea9b57d8c382714b4f2f00911fb151f3a905d
newrav1k <kritsky.academi@gmail.com>
2025-09-11 00:08:12 +0300

    Added transaction cancelled event and update transaction event publisher

--- transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/event/publisher/TransactionEventPublisher.java
+++ transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/event/publisher/TransactionEventPublisher.java
@@ -2,12 +2,14 @@ package ru.mirea.newrav1k.transactionservice.event.publisher;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
+import ru.mirea.newrav1k.transactionservice.configuration.properties.TransactionTopicsProperties;
 import ru.mirea.newrav1k.transactionservice.event.BalanceUpdateFailureEvent;
+import ru.mirea.newrav1k.transactionservice.event.TransactionCancelledEvent;
 import ru.mirea.newrav1k.transactionservice.event.TransactionCreatedEvent;
 import ru.mirea.newrav1k.transactionservice.event.TransactionSuccessCreatedEvent;
 import ru.mirea.newrav1k.transactionservice.model.entity.Transaction;
+import ru.mirea.newrav1k.transactionservice.service.OutboxService;
 
 import java.util.UUID;
 
@@ -16,37 +18,61 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class TransactionEventPublisher {
 
-    private final ApplicationEventPublisher eventPublisher;
+    private static final String AGGREGATE_TYPE = "Transaction";
+
+    private final OutboxService outboxService;
+
+    private final TransactionTopicsProperties topics;
 
     public void publishTransactionCreatedEvent(Transaction transaction) {
-        log.debug("Publishing transaction created event");
-        TransactionCreatedEvent createdEvent = new TransactionCreatedEvent(
+        log.debug("Publishing TransactionCreatedEvent");
+        TransactionCreatedEvent event = new TransactionCreatedEvent(
                 UUID.randomUUID(),
                 transaction.getId(),
                 transaction.getAccountId(),
                 transaction.getType(),
                 transaction.getAmount()
         );
-        this.eventPublisher.publishEvent(createdEvent);
+
+        this.outboxService.saveEvent(AGGREGATE_TYPE, transaction.getId(),
+                this.topics.transactionCreated(), event.getClass().getSimpleName(), event);
+    }
+
+    public void publishTransactionCancelledEvent(Transaction transaction) {
+        log.debug("Publishing TransactionCancelledEvent");
+        TransactionCancelledEvent event = new TransactionCancelledEvent(
+                UUID.randomUUID(),
+                transaction.getId(),
+                transaction.getAccountId(),
+                transaction.getType(),
+                transaction.getAmount()
+        );
+
+        this.outboxService.saveEvent(AGGREGATE_TYPE, transaction.getId(),
+                this.topics.transactionCancelled(), event.getClass().getSimpleName(), event);
     }
 
     public void publishTransactionSuccessCreatedEvent(UUID transactionId) {
-        log.debug("Publishing transaction success created event");
-        TransactionSuccessCreatedEvent successCreatedEvent = new TransactionSuccessCreatedEvent(
+        log.debug("Publishing TransactionSuccessCreatedEvent");
+        TransactionSuccessCreatedEvent event = new TransactionSuccessCreatedEvent(
                 UUID.randomUUID(),
                 transactionId
         );
-        this.eventPublisher.publishEvent(successCreatedEvent);
+
+        this.outboxService.saveEvent(AGGREGATE_TYPE, transactionId,
+                this.topics.transactionSuccessfullyCreated(), event.getClass().getSimpleName(), event);
     }
 
     public void publishBalanceUpdateFailureEvent(UUID transactionId, UUID accountId) {
-        log.debug("Publishing balance update failure event");
-        BalanceUpdateFailureEvent failureEvent = new BalanceUpdateFailureEvent(
+        log.debug("Publishing BalanceUpdateFailureEvent");
+        BalanceUpdateFailureEvent event = new BalanceUpdateFailureEvent(
                 UUID.randomUUID(),
                 transactionId,
                 accountId
         );
-        this.eventPublisher.publishEvent(failureEvent);
+
+        this.outboxService.saveEvent(AGGREGATE_TYPE, transactionId,
+                this.topics.transactionBalanceFailure(), event.getClass().getSimpleName(), event);
     }
 
 }
