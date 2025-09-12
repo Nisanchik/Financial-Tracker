ca9d202fe0a9aa92dff4d23d6ff6260781be05c9
newrav1k <kritsky.academi@gmail.com>
2025-09-12 17:33:43 +0300

    Fixed an error when starting the transaction-service and added a server for kafka in application.yaml

--- transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/TransactionServiceApplication.java
+++ transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/TransactionServiceApplication.java
@@ -1,9 +1,11 @@
 package ru.mirea.newrav1k.transactionservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@SpringBootApplication
 @EnableScheduling
 @ConfigurationPropertiesScan
 public class TransactionServiceApplication {
