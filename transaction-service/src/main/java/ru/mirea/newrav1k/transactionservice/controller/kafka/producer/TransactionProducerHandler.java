d9b4cb8ef860c3c4c836a4a90984a9d4aded7fbb
newrav1k <kritsky.academi@gmail.com>
2025-09-13 08:27:26 +0300

    Added sent method to TransactionProducerHandler

--- transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/controller/kafka/producer/TransactionProducerHandler.java
+++ transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/controller/kafka/producer/TransactionProducerHandler.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import ru.mirea.newrav1k.transactionservice.model.entity.OutboxEvent;
+
+import java.util.concurrent.ExecutionException;
 
 @Slf4j
 @Component
@@ -11,5 +14,9 @@ import org.springframework.stereotype.Component;
 public class TransactionProducerHandler {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void send(OutboxEvent event) throws ExecutionException, InterruptedException {
+        this.kafkaTemplate.send(event.getTopic(), event.getAggregateId().toString(), event).get();
+    }
 
 }
