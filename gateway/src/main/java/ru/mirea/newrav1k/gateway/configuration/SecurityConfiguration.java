f0f35b49ac3734a7db3b5534290c7d2e6679a12e
newrav1k <kritsky.academi@gmail.com>
2025-09-20 02:53:53 +0300

    Fixed jwt-set-uri and added algorithm

--- gateway/src/main/java/ru/mirea/newrav1k/gateway/configuration/SecurityConfiguration.java
+++ gateway/src/main/java/ru/mirea/newrav1k/gateway/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
@@ -27,7 +28,9 @@ public class SecurityConfiguration {
 
     @Bean
     public ReactiveJwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri) {
-        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri)
+                .jwsAlgorithm(SignatureAlgorithm.RS256)
+                .build();
     }
 
 }
