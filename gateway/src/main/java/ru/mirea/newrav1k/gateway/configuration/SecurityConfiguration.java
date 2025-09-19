1b91ccfe17dd11159a1889968bcb87d3ace1968b
newrav1k <kritsky.academi@gmail.com>
2025-09-19 22:19:44 +0300

    Added redis configuration, caching to services

---
+++ gateway/src/main/java/ru/mirea/newrav1k/gateway/configuration/SecurityConfiguration.java
@@ -0,0 +1,33 @@
+package ru.mirea.newrav1k.gateway.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(authorizeExchangeSpec ->
+                        authorizeExchangeSpec.anyExchange().permitAll())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec ->
+                        oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()));
+        return http.build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder(@Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}") String jwkSetUri) {
+        return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri).build();
+    }
+
+}
---
+++ gateway/src/main/java/ru/mirea/newrav1k/gateway/security/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,32 @@
+package ru.mirea.newrav1k.gateway.security.filter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends AbstractGatewayFilterFactory<Object> {
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> exchange.getPrincipal()
+                .filter(JwtAuthenticationToken.class::isInstance)
+                .cast(JwtAuthenticationToken.class)
+                .map(JwtAuthenticationToken::getToken)
+                .map(jwt -> {
+                    ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                            .header("X-User-Id", jwt.getSubject())
+                            .header("X-User-Authorities", String.join(",",
+                                    jwt.getClaimAsStringList("authorities")))
+                            .build();
+                    return exchange.mutate().request(mutatedRequest).build();
+                })
+                .defaultIfEmpty(exchange)
+                .flatMap(chain::filter);
+    }
+
+}
