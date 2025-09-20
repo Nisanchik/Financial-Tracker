9c80d36d443d8e6c5229b5a898d83e99770f3450
newrav1k <kritsky.academi@gmail.com>
2025-09-20 16:38:50 +0300

    Added the method to the repository

--- account-service/src/main/java/ru/mirea/newrav1k/accountservice/repository/AccountRepository.java
+++ account-service/src/main/java/ru/mirea/newrav1k/accountservice/repository/AccountRepository.java
@@ -1,6 +1,8 @@
 package ru.mirea.newrav1k.accountservice.repository;
 
 import jakarta.persistence.LockModeType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
@@ -13,8 +15,14 @@ import java.util.UUID;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
 
+    Page<Account> findAllByUserId(UUID userId, Pageable pageable);
+
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select a from Account a where a.id = :accountId")
-    Optional<Account> findByIdForPessimisticLock(UUID accountId);
+    @Query("select a from Account a where a.id = :accountId and a.userId = :userId")
+    Optional<Account> findAccountByUserIdAndIdForPessimisticLock(UUID userId, UUID accountId);
+
+    Optional<Account> findAccountByUserIdAndId(UUID userId, UUID accountId);
+
+    void deleteAccountByUserIdAndId(UUID userId, UUID accountId);
 
 }
