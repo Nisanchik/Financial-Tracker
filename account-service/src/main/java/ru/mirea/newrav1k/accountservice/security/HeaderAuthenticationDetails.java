5bc60379d8778b5a9e0e4ca96d318d993fc7f56c
newrav1k <kritsky.academi@gmail.com>
2025-09-20 16:07:32 +0300

    Added dependency on Spring Security and implemented header authentication

---
+++ account-service/src/main/java/ru/mirea/newrav1k/accountservice/security/HeaderAuthenticationDetails.java
@@ -0,0 +1,29 @@
+package ru.mirea.newrav1k.accountservice.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class HeaderAuthenticationDetails implements UserDetails {
+
+    private UUID userId;
+
+    private List<GrantedAuthority> authorities;
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+}
