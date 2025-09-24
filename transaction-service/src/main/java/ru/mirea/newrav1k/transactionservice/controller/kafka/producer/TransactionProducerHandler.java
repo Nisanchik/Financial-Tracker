a153a8c5ffde9d543d447497dd10e20bc18b6fe3
newrav1k <kritsky.academi@gmail.com>
2025-09-24 22:26:52 +0300

    Improved transaction events sending in kafka

--- transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/controller/kafka/producer/TransactionProducerHandler.java
+++ transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/controller/kafka/producer/TransactionProducerHandler.java
@@ -1,12 +1,15 @@
 package ru.mirea.newrav1k.transactionservice.controller.kafka.producer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 import ru.mirea.newrav1k.transactionservice.model.entity.OutboxEvent;
 
-import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @Component
@@ -15,8 +18,32 @@ public class TransactionProducerHandler {
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
 
-    public void send(OutboxEvent event) throws ExecutionException, InterruptedException {
-        this.kafkaTemplate.send(event.getTopic(), event.getAggregateId().toString(), event).get();
+    private final ObjectMapper objectMapper;
+
+    public void send(OutboxEvent event) {
+        log.debug("Sending event: event={}, topic={}", event, event.getTopic());
+        try {
+            Class<?> clazz = Class.forName("ru.mirea.newrav1k.transactionservice.event." + event.getEventType());
+            Object object = this.objectMapper.readValue(event.getPayload(), clazz);
+            this.kafkaTemplate.send(event.getTopic(), event.getAggregateId().toString(), object)
+                    .get(10L, TimeUnit.SECONDS);
+            log.info("Successfully sent event: event={}, topic={}", event, event.getTopic());
+        } catch (ClassNotFoundException exception) {
+            log.error("Class not found: class={}", event.getEventType(), exception);
+            throw new IllegalArgumentException("Event class not found", exception);
+        } catch (JsonProcessingException exception) {
+            log.error("Could not convert event to JSON: class={}, payload={}", event.getEventType(), event.getPayload(), exception);
+            throw new IllegalArgumentException("Event payload could not be converted to JSON", exception);
+        } catch (TimeoutException exception) {
+            log.error("Timed out: class={}, payload={}", event.getEventType(), event.getPayload(), exception);
+            throw new IllegalArgumentException("Timed out", exception);
+        } catch (InterruptedException exception) {
+            log.error("Interrupted exception: event={}", event, exception);
+            throw new IllegalArgumentException("Interrupted while sending", exception);
+        } catch (Exception exception) {
+            log.error("Unexpected exception: event={}", event, exception);
+            throw new IllegalArgumentException("Exception while sending", exception);
+        }
     }
 
 }
