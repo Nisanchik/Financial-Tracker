3b6df5a6d8ec4208d70b91a5e358d098734bb142
newrav1k <kritsky.academi@gmail.com>
2025-09-24 21:52:14 +0300

    RequestInterceptor has been added to add information about the authenticated user to the headers

--- transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/configuration/FeignConfiguration.java
+++ transaction-service/src/main/java/ru/mirea/newrav1k/transactionservice/configuration/FeignConfiguration.java
@@ -1,10 +1,36 @@
 package ru.mirea.newrav1k.transactionservice.configuration;
 
+import feign.RequestInterceptor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import ru.mirea.newrav1k.transactionservice.security.HeaderAuthenticationDetails;
 
+import java.util.stream.Collectors;
+
+@Slf4j
 @Configuration
 @EnableFeignClients(basePackages = "ru.mirea.newrav1k.transactionservice.service.client")
 public class FeignConfiguration {
+
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            Authentication authentication = securityContext.getAuthentication();
+            if (authentication != null && authentication.isAuthenticated()) {
+                HeaderAuthenticationDetails authenticationDetails = (HeaderAuthenticationDetails) authentication.getPrincipal();
+                requestTemplate.header("X-Tracker-Id", authenticationDetails.getTrackerId().toString());
+                requestTemplate.header("X-Tracker-Authorities", authentication.getAuthorities().stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .collect(Collectors.joining(", ")));
+            }
+        };
+    }
 
 }
